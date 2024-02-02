c0ce9cc80a74c45ab5b6abc0a6f5a679af3ee28a
Sami Hakani <sami.hakani@gmail.com>
2024-02-01 16:41:23 -0800

    Created grid with HTML injection

--- frontend/app.py
+++ frontend/app.py
@@ -10,59 +10,29 @@ def buttonGrid(n: int, m: int) -> None:
         i += 1
         
 def buttonGrid2(n: int, m: int) -> None:
+    markdown_str = ''
+    for row in range(n):
+        for col in range(m):
+            markdown_str += '<input type="checkbox"></input>'
+    markdown_str = f'<div class="moonboard">{markdown_str}</div>'
+    st.markdown(markdown_str, unsafe_allow_html=True)
     st.markdown("""
-<script>
-let moonboard = document.createElement('div');
-moonboard.classList.add('.moonboard')
-
-// let moonboard = document.querySelector('.moonboard');
-
-let selected_holds = [];
-
-/* * * * * * * * * * * * * * * * * * * * * * 
-
-                DOM CREATION 
-
-* * * * * * * * * * * * * * * * * * * * * */
-
-for (let i = 0; i < 20; i++) {
-    let row = document.createElement('div');
-    row.classList.add(`row-${i}`);
-    for(let j = 0; j < 10; j++) {
-        let btn = document.createElement('button');
-        btn.classList.add(`hold`);
-        btn.classList.add(`btn-${i}-${j}`);
-
-        // let buttonLabel = buttonMapping[i][j];
-        // let buttonLabel = `button-${i}-${j}`;
-        let buttonLabel = 'hold';
-
-        btn.textContent = buttonLabel;
-        // if (Array.from('1234567890+-*/').includes(buttonLabel)) { // numbers and operations
-        //     btn.addEventListener('click', () => appendDisplayValue(buttonLabel));
-        // }
-
-        row.appendChild(btn);
-    }
-    moonboard.appendChild(row);
-}
-</script>
-                """, unsafe_allow_html=True)
-    # st.markdown(f'<div class="moonboard">', unsafe_allow_html=True)
-    # for i in range(n):
-    #     for j in range(m):
-    #         st.markdown(f'<input type="checkbox"> {i},{j} </input>', unsafe_allow_html=True)
-    
-    # st.markdown("""
-    #              <style>
-    #              input {
-    #                 padding: 100px; 
-    #              }
-    #              </style>
-    #              """, unsafe_allow_html=True)   
-    # st.markdown(f'</div>', unsafe_allow_html=True)
+                <style>
+                .moonboard {
+                    display: grid;
+                    grid-gap: 10px;
+                    grid-template-columns: repeat(10, 1fr);
+                    grid-template-rows: repeat(20, 1fr);
+                }
+                
+                input {
+                    width: 50px;
+                    height: 50px;
+                }
+                </style>
+                """,unsafe_allow_html=True)
 
 st.title("Route Grader")
 
 # buttonGrid(20, 10)
-buttonGrid2(2,2)
+buttonGrid2(20,10)
