7225991e669d4abe0b06c9b3bcaf52446f1bb8a6
Sami Hakani <sami.hakani@gmail.com>
2024-02-19 16:42:34 -0500

    Parser works

--- frontend/df_parser.py
+++ frontend/df_parser.py
@@ -7,5 +7,5 @@ def parse(df: pd.DataFrame):
     (0, 0) -> A18; (18, A)
     (17, 10) -> K1; (1, K)
     """
-    values = [chr(ord('A') + j) + f"{18 - i}" for i in range(len(values)) for j in range(len(values[0])) if values[i][j]]
+    values = [(chr(ord('A') + j) + f"{18 - i}", 'hold') for i in range(len(values)) for j in range(len(values[0])) if values[i][j]]
     return values
