bc2853ee96e99d82d0695f47b934037abc9b6918
Sami Hakani <sami.hakani@gmail.com>
2024-02-19 16:39:37 -0500

    Parser now reads holds

--- frontend/df_parser.py
+++ frontend/df_parser.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 def parse(df: pd.DataFrame):
     res = []
-    values = df.values()
-    values = [(i, j) for i in range(len(values)) for j in range(len(values[0])) if values[i][j]]
+    values = df.values
+    """
+    (0, 0) -> A18; (18, A)
+    (17, 10) -> K1; (1, K)
+    """
+    values = [chr(ord('A') + j) + f"{18 - i}" for i in range(len(values)) for j in range(len(values[0])) if values[i][j]]
     return values
