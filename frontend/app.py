f6a985a2b89767e6096b90cc58068c299ccbc5bb
Sami Hakani <sami.hakani@gmail.com>
2024-02-19 16:22:27 -0500

    Using data_editor instead of button grid

--- frontend/app.py
+++ frontend/app.py
@@ -1,35 +1,9 @@
 import streamlit as st 
 import streamlit.components.v1 as components
+import pandas as pd
         
-def buttonGrid(n: int, m: int) -> None:
-    markdown_str = ''
-    for row in range(n):
-        for col in range(m):
-            markdown_str += f'<input type="checkbox" id="hold-{row}-{col}" class="hold"></input>'
-    markdown_str = f'<div class="moonboard">{markdown_str}</div>'
-    st.markdown(markdown_str, unsafe_allow_html=True)
-    st.markdown("""
-                <style>
-                .moonboard {
-                    display: grid;
-                    grid-gap: 10px;
-                    grid-template-columns: repeat(10, 1fr);
-                    grid-template-rows: repeat(20, 1fr);
-                }
-                
-                input {
-                    width: 20px;
-                    height: 20px;
-                }
-                </style>
-                """,unsafe_allow_html=True)
 st.title("Route Grader")
-gradeButton = st.button("Grade This Route")
 
-if gradeButton:
-    st.markdown("""<script>
-    console.log("hello");
-    
-                    </script>""", unsafe_allow_html=True)
-
-buttonGrid(20,10)
+df = pd.DataFrame([{f"col-{col}" : False for col in range(10)} for row in range(20)])
+edited_df = st.data_editor(df)
+st.markdown(edited_df.values)
