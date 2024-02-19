1bcfd2467c6964dd3ddc74b0aecfe92f3fef68e7
Sami Hakani <sami.hakani@gmail.com>
2024-02-19 16:39:57 -0500

    changed frontend to parse df rather than df.values

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,14 @@
 import streamlit as st 
 import streamlit.components.v1 as components
 import pandas as pd
+from df_parser import parse
         
 st.title("Route Grader")
 
 grade = st.button("Grade Route")
 
-df = pd.DataFrame([{f"col-{col}" : False for col in range(10)} for row in range(20)])
+df = pd.DataFrame([{f"col-{col}" : False for col in range(11)} for row in range(18)])
 edited_df = st.data_editor(df)
 
 if grade:
-    st.markdown(edited_df.values)
+    st.markdown(parse(edited_df))
