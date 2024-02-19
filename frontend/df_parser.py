f66f4812704de38049bb1d2524111e6083e98000
Sami Hakani <sami.hakani@gmail.com>
2024-02-19 16:32:25 -0500

    Parser converts df to list of tuples

--- frontend/df_parser.py
+++ frontend/df_parser.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+def parse(df: pd.DataFrame):
+    res = []
+    values = df.values()
+    values = [(i, j) for i in range(len(values)) for j in range(len(values[0])) if values[i][j]]
+    return values
